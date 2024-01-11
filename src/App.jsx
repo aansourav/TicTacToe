33351686344aa0ba22709d315e9abe52243f8ec1
SoUrAv <aansourav@gmail.com>
2024-01-11 18:57:34 +0600

    Beginning

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+const Square = () => {
+  return (
+    <>
+      <div>
+        <button className="bg-white border border-gray-500 h-12 w-12 m-1 leading-9 text-lg" >X</button>
+        <button className="bg-white border border-gray-500 h-12 w-12 m-1 leading-9 text-lg" >X</button>
+        <button className="bg-white border border-gray-500 h-12 w-12 m-1 leading-9 text-lg" >X</button>
+      </div>
+      <div>
+        <button className="bg-white border border-gray-500 h-12 w-12 m-1 leading-9 text-lg" >X</button>
+        <button className="bg-white border border-gray-500 h-12 w-12 m-1 leading-9 text-lg" >X</button>
+        <button className="bg-white border border-gray-500 h-12 w-12 m-1 leading-9 text-lg" >X</button>
+      </div>
+      <div>
+        <button className="bg-white border border-gray-500 h-12 w-12 m-1 leading-9 text-lg" >X</button>
+        <button className="bg-white border border-gray-500 h-12 w-12 m-1 leading-9 text-lg" >X</button>
+        <button className="bg-white border border-gray-500 h-12 w-12 m-1 leading-9 text-lg" >X</button>
+      </div>
+    </>
+  )
+}
+
+export default Square;
