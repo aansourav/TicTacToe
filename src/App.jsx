5f766884c69e6e26b186509bb24edae596dca7dc
SoUrAv <aansourav@gmail.com>
2024-01-12 12:30:53 +0600

    Status color edited

--- src/App.jsx
+++ src/App.jsx
@@ -83,9 +83,9 @@ export default function Game() {
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = `Go to the move # ${move}`;
+      description = `Go to move # ${move}`;
     } else {
-      description = `Go to start the game`;
+      description = `Go to start of the game`;
     }
     return (
       <li
